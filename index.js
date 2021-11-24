952d595f208e30863a04660e9aa7e85c3ca271e9
Joe McPartland <joemcpartland@gmail.com>
2021-11-24 16:59:50 -0700

    commit tree 8b869bf41048868153031e6c36823348d8444d05

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+// function hasTargetSum(myArray, target) {
+//   for (let i = 0; i < myArray.length; i++) {
+//     for (let j = 1; j < myArray.length; j++) {
+//       if (myArray[i] + myArray[j] === target) {
+//         // console.log(myArray[i] + myArray[j])
+//         return true
+//       } 
+//     }
+//   }
+// };
+
+
+
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  const numCheck = {};
+  for (const number of array) {
+    const complement = target - number;
+    if (complement in numCheck) return true;
+    numCheck[number] = true;
+  }
+  return false;
+};
+
+
+
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +42,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  function (array, target) {
+    if the sum of 2 integers in the array = target
+      return true
+    else
+      false
+  }
 */
 
 /*
@@ -32,3 +73,9 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
+
+// Create function that takes in an array of integers and a target integer
+// check if 2 integers of the array add up to the target integer
+// return true if 2 integers add up to the target and return false otherwise
---
+++ index1.js
@@ -0,0 +1,4 @@
+const i = [1,2,3,4];
+const j = [2,3,4,5];
+
+console.log(j[1] + i[1])
